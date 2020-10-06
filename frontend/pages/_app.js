f1a5d4c7ff0510d80bf5dd6bcbb304f027074f90
nathanluu <nate1299@me.com>
2020-10-06 16:54:53 -0700

    Added Tailwind CSS, PurgeCSS, and the Inter Web Font

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ frontend/pages/_document.js
@@ -0,0 +1,25 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {/*Inter Web Font*/}
+                    <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
+                </Head>
+                <body>
+                <Main />
+                <NextScript />
+                </body>
+            </Html>
+        )
+    }
+}
+
+export default MyDocument
