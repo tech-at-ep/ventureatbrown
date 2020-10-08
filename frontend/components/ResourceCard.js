2992204fc22f10f03d0d0e21d13e2203f79db962
jaspaaaaaa <jasper20020202@gmail.com>
2020-10-07 17:59:13 -0700

    synced resource card style with question card style

--- frontend/components/ResourceCard.js
+++ frontend/components/ResourceCard.js
@@ -5,16 +5,31 @@ export default function ({item, onResetSurvey}) {
 
     if (!resource) return (<>
         <h1>There aren't any resources yet to display. Create the <span className="p-1 bg-gray-100 font-mono">{item}</span> resource!</h1>
-        <button onClick={onResetSurvey} className="text-blue-400 mt-4">Start over</button>
     </>)
 
     return <div>
-        <h1 className="text-2xl font-bold">{resource.title}</h1>
-        <div class="divide-y-2 divide-red-500 divide-dashed">
+                <h1 className="text-5xl flex font-bold text-gray-900 block w-auto ">
+                    <div className="m-7 mt-2 p-3 pt-5">
+                        <svg className="fill-current text-brown-700 w-5 h-4">
+                                <path d="M0 0 L20 0 L20 20 L0 20 Z"/>
+                        </svg> 
+                    </div>
+                    <div>{resource.title}</div>
+                </h1>
+
             {resource.resources.map((item, index) => <div key={`resourceItem_${index}`}>
-                <a href={item.url}><span class="transition duration-500 ease-in-out bg-transparent hover:bg-yellow-300">{item.name}</span></a>
+                <a href={item.url}>
+                    <span className="group block p-0 hover:text-red-700 focus:underline p-3
+                    bg-gradient-to-r  hover:from-red-50
+                    text-gray-800 rounded-lg text-xl font-medium">
+                        <div className="inline-block ml-3 mr-3">
+                            <svg className="fill-current text-red-700 w-5 h-5">
+                                <path d="M0 0 L15 10 L0 20 Z"/>
+                            </svg>
+                        </div>
+                            {item.name}
+                    </span>
+                    </a>
             </div>)}
-        </div>
-        <button onClick={onResetSurvey} className="text-blue-400 mt-4">Start over</button>
     </div>
 }
