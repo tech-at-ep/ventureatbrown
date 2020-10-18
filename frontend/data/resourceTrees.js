dd1ce5c19694d7528b60cb802dc89da9f690dbed
Abby Powell <abigail_powell@brown.edu>
2020-10-18 17:59:17 -0400

    Added all resources

--- frontend/data/resourceTrees.js
+++ frontend/data/resourceTrees.js
@@ -13,6 +13,41 @@ export const resourceTrees = {
                 url: "https://brownrisdinnovation.com/",
                 // Use image URLS if preferable
                 image: "https://brownrisdinnovation.com/static/media/landing.f241e293.png"
+            },
+            {
+                name: "Design@Brown",
+                url: "https://www.designatbrown.com/",
+                image: "https://www.brown.edu/research/misc/favicon.ico" //Shield - cannot find image
+            },
+            {
+                name: "RISD E\'Ship",
+                url: "https://www.risdeship.com/",
+                image: "https://images.squarespace-cdn.com/content/v1/5bde1df7cc8fed0d1c18b414/1550415702706-MCHUI2XL7JO6XF89J98M/ke17ZwdGBToddI8pDm48kOwzcN5TZyCHfHH4ghCAAPxZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpw0y3kSdl97pdUWtoyKq9MNhNDi3n0ltJOAocqtrjqcZC1dznSDY9o9ffoXy3aATx4/%23RAW_LOGO-01+copy.png"
+            },
+            {
+                name: "Design for America",
+                url: "https://designforamerica.com/category/risd-brown/",
+                image: "https://i1.wp.com/designforamerica.com/wp-content/uploads/2016/04/cropped-favicon-e1459784555889.png?fit=512%2C512"
+            },
+            {
+                name: "Better World by Design",
+                url: "http://www.betterworldxdesign.com/",
+                image: "https://www.brown.edu/sustainability/sites/initiatives-brown-is-green/files/images/BWD2.png"
+            },
+            {
+                name: "RISD Industrial Design Society of America",
+                url: "https://www.facebook.com/risdidsa/",
+                image: "https://freight.cargo.site/t/original/i/017f7634db61edee95b66d000603fe3a395798b6ba48795c20897b6af2f2c0ea/RISD_IDSA_Logo_VFinal-1.png"
+            },
+            {
+                name: "Full Stack at Brown",
+                url: "https://www.fullstackatbrown.com/",
+                image: "https://media-exp1.licdn.com/dms/image/C560BAQGtghHFHJLm3Q/company-logo_200_200/0?e=2159024400&v=beta&t=pWk2QEJ8wiZg1G1A_Kfax6GONcYUBDEy7-7MiWdHgCI"
+            },
+            {
+                name: "Brown/ RISD STEAM",
+                url: "https://www.facebook.com/brownuniversitysteam/",
+                image: "https://www.brown.edu/research/misc/favicon.ico" //Shield - cannot find image
             }
             // ...
         ]
@@ -30,6 +65,31 @@ export const resourceTrees = {
                 url: "https://medium.com/unshackled-ventures/launching-our-university-fellowship-3-0-3a975c7c8e0e",
                 image: "https://miro.medium.com/max/1400/1*O7I1b8bR1MG5uhapcwvXqg.png"
             }
+            {
+                name: "Blueprint Investor Track",
+                url: "https://blueprint.dormroomfund.com/",
+                image: "https://images.typeform.com/images/Xvh3BHszwvxH/image/default"
+            },
+            {
+                name: ".406 Venture Fellow",
+                url: "https://www.406ventures.com/about",
+                image: "https://res-1.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/v1435310035/vmm6ugjlhyps9yhu3ufi.png"
+            },
+            {
+                name: "Rough Draft Fellowship",
+                url: "https://medium.com/rough-draft-ventures/join-the-rough-draft-ventures-team-83fb45e6415b",
+                image: "https://entrepreneurship.brown.edu/wp-content/uploads/2018/10/rough-draft-ventures-e1539801834693.jpg"
+            },
+            {
+                name: "Contrary Partner",
+                url: "https://contrarycap.com/join",
+                image: "https://global-uploads.webflow.com/5edc35fae93ebb8456425f57/5edd3639e5cf1e9d5b597359_f39hub76mlsoh4pqnxgx.png"
+            },
+            {
+                name: "Van Wickle Ventures",
+                url: "https://www.vanwickleventures.com/",
+                image: "https://images.squarespace-cdn.com/content/v1/5caa50d0af4683c75a73decb/1556564892816-7PIBFHDPYNI1M8LQ9XEK/ke17ZwdGBToddI8pDm48kGfiFqkITS6axXxhYYUCnlRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxQ1ibo-zdhORxWnJtmNCajDe36aQmu-4Z4SFOss0oowgxUaachD66r8Ra2gwuBSqM/unnamed-1.png"
+            }
             // ...
         ]
     },
@@ -43,13 +103,13 @@ export const resourceTrees = {
             },
             {
                 name: "Entrepreneurship Certificate",
-                url: "https://entrepreneurship.brown.edu/entrepreneurship-certificate/", 
-                image: "https://www.brown.edu/research/misc/favicon.ico" 
+                url: "https://entrepreneurship.brown.edu/entrepreneurship-certificate/",
+                image: "https://www.brown.edu/research/misc/favicon.ico"
             },
             {
                 name: "Brown Technology Innovations",
                 url: "https://www.brown.edu/research/tech-innovations",
-                image: "https://www.brown.edu/research/misc/favicon.ico" 
+                image: "https://www.brown.edu/research/misc/favicon.ico"
             },
             {
                 name: "PRIME (Master's Degree)",
@@ -64,7 +124,7 @@ export const resourceTrees = {
             {
                 name: "Social Innovation Fellowship",
                 url: "https://www.brown.edu/academics/college/swearer/programs/social-innovation-fellowship/11",
-                image: "https://www.brown.edu/research/misc/favicon.ico" 
+                image: "https://www.brown.edu/research/misc/favicon.ico"
             }
         ]
     },
@@ -89,7 +149,7 @@ export const resourceTrees = {
            {
                name: "Contrary Talent",
                url: "https://contrarycap.com/talent",
-               image: ""
+               image: "https://global-uploads.webflow.com/5edc35fae93ebb8456425f57/5edd3639e5cf1e9d5b597359_f39hub76mlsoh4pqnxgx.png"
            },
            {
                name: "IDE0 CoLab Fellowship",
@@ -107,6 +167,211 @@ export const resourceTrees = {
                image: "https://images.squarespace-cdn.com/content/v1/5eaa33992b45c716a1359bd8/1588737273472-HGWPNESSJ8HEIWRCG3S5/ke17ZwdGBToddI8pDm48kNFfjQoK8u46QcnvnhWRo8hZw-zPPgdn4jUwVcJE1ZvWEtT5uBSRWt4vQZAgTJucoTqqXjS3CfNDSuuf31e0tVG6aNyXC6xmNUia-8AE5hjT-redPsGBFjlrkWlDWo0FnZuG45vQwBxdpDrCGUSSl5w/favicon.ico?format=100w"
            },
        ]
+   },
+    conferencesAndTrips: {
+        title: "Conferences and Trips",
+        resources: [
+            {
+                name: "Startup@Brown",
+                url: "https://www.facebook.com/startupatbrown/",
+                image: "https://pbs.twimg.com/profile_images/621189395022917632/iayfgleS_400x400.png"
+            },
+            {
+                name: "Better World by Design",
+                url: "http://www.betterworldxdesign.com/",
+                image: "https://www.brown.edu/sustainability/sites/initiatives-brown-is-green/files/images/BWD2.png"
+            },
+            {
+                name: "WE@Brown",
+                url: "https://www.brownentrepreneurship.com/we-at-brown",
+                image: "https://www.logolynx.com/images/logolynx/82/82b071a861026ee136cfe249798b2b34.png"
+            },
+            {
+                name: "Future of Sustainable Investing",
+                url: "https://www.fsicon.com/",
+                image: "https://images.squarespace-cdn.com/content/5d8937d2aaeb04772b7efdc6/1569716138652-R63C4ZCQ0PGZ3I8W12IV/grayback.png?content-type=image%2Fpng"
+            },
+            {
+                name: "Synapse Trips",
+                url: "https://entrepreneurship.brown.edu/beyond-campus/",
+                image: "https://www.logolynx.com/images/logolynx/82/82b071a861026ee136cfe249798b2b34.png"
+            }
+        ]
+    },
+    earlyIdeaCompetitions: {
+        title: "Early Idea Competitions",
+        resources: [
+            {
+                name: "WE@Brown Pitch",
+                url: "https://www.brownentrepreneurship.com/we-at-brown",
+                image: "https://www.logolynx.com/images/logolynx/82/82b071a861026ee136cfe249798b2b34.png"
+            },
+            {
+                name: "Hack@Brown",
+                url: "https://2020.hackatbrown.org/",
+                image: "https://lh3.googleusercontent.com/proxy/eqTbo0mp805xpreVeIgZBfouW37YXMbPTY-tRrXWRBKBLfIWiODBIDThgaI6RCPRHC-j8RZ42f-Z_Lbeaerix6RRhVsIL4Ada_1NB-5lEaMXaGge014YhogqxPw"
+            },
+            {
+                name: "Innovation Dojo",
+                url: "https://www.brownentrepreneurship.com/dojo",
+                image: "https://www.logolynx.com/images/logolynx/82/82b071a861026ee136cfe249798b2b34.png"
+            },
+            {
+                name: "Hack@Home",
+                url: "https://hackathome.org/",
+                image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSF4UwabUoZ-e7FXkGO7ouqSYZR1qXD7DoW9Q&usqp=CAU"
+            }
+        ]
+    },
+    earlyStageAcceleratorsAndSupport: {
+        title: "Early Stage Accelerators and Support",
+        resources: [
+            {
+                name: "MassChallenge",
+                url: "https://masschallenge.org/programs-rhode-island",
+                image: "https://yt3.ggpht.com/a/AATXAJzJx0ow-NYBrqrGHkpINIb7T_gRh9RnIT6nBZ3diw=s900-c-k-c0x00ffffff-no-rj"
+            },
+            {
+                name: "Contrary Capital Accelerator",
+                url: "https://contrarycap.com/accelerator", //This url does not work
+                image: "https://global-uploads.webflow.com/5edc35fae93ebb8456425f57/5edd3639e5cf1e9d5b597359_f39hub76mlsoh4pqnxgx.png"
+            },
+            {
+                name: "Breakthrough Lab",
+                url: "https://entrepreneurship.brown.edu/b-lab/",
+                image: "https://www.brown.edu/research/misc/favicon.ico" //shield
+            },
+            {
+                name: "Envision Accelerator",
+                url: "https://www.envisionaccelerator.com/",
+                image: "https://uploads-ssl.webflow.com/5ee65e27175d3364456c1f25/5eed1b9d3a2b7b9170388705_graph.png"
+            },
+            {
+                name: "Female Founders Track (DRF)",
+                url: "https://femalefounders.dormroomfund.com/",
+                image: "https://femalefounders.dormroomfund.com/img/fff_logo.svg"
+            },
+            {
+                name: "Blueprint Founders Track (DRF)",
+                url: "https://blueprint.dormroomfund.com/",
+                image: "https://images.typeform.com/images/Xvh3BHszwvxH/image/default"
+            },
+            {
+                name: "Community Lab @ Nelson",
+                url: "https://entrepreneurship.brown.edu/community-lab/",
+                image: "https://www.brown.edu/research/misc/favicon.ico" //shield
+            },
+            {
+                name: "Sustainable Enterprise Greenhouse",
+                url: "https://segreenhouse.org/",
+                image: "https://segreenhouse.org/wp-content/themes/seg/images/logo-greenblue.png"
+            },
+            {
+                name: "YC Startup School",
+                url: "https://www.startupschool.org/",
+                image: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Y_Combinator_logo.svg/1200px-Y_Combinator_logo.svg.png"
+            },
+            {
+                name: "CIC",
+                url: "https://cic.com/providence",
+                image: "https://image.spreadshirtmedia.com/image-server/v1/designs/1008711011,width=300,height=300,backgroundColor=666666/cic-pixel-logo.jpg"
+            },
+            {
+                name: "Venture Cafe",
+                url: "https://districthallprovidence.org/",
+                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F88878237%2F331118989883%2F1%2Foriginal.jpg?w=225&auto=format%2Ccompress&q=75&sharp=10&s=09ef528de2912dc903c46ba700dcd42b"
+            }
+        ]
+    },
+    studentVcs: {
+        title: "Student VCs",
+        resources: [
+            {
+                name: "Dorm Room Fund",
+                url: "https://www.dormroomfund.com/",
+                image: "https://join.dormroomfund.com/groupshot.JPG"
+            },
+            {
+                name: "Contrary Capital",
+                url: "https://contrarycap.com/",
+                image: "https://global-uploads.webflow.com/5edc35fae93ebb8456425f57/5edd3639e5cf1e9d5b597359_f39hub76mlsoh4pqnxgx.png"
+            },
+            {
+                name: "Rough Draft Ventures",
+                url: "https://www.roughdraft.vc/",
+                image: "https://entrepreneurship.brown.edu/wp-content/uploads/2018/10/rough-draft-ventures-e1539801834693.jpg"
+            },
+            {
+                name: "Pear VC",
+                url: "https://www.pear.vc/".
+                image: "https://i.pinimg.com/originals/c1/d0/25/c1d0252568f7db2120d785e726e251e2.png"
+            },
+            {
+                name: "Van Wickle Ventures",
+                url: "https://www.vanwickleventures.com/",
+                image: "https://images.squarespace-cdn.com/content/v1/5caa50d0af4683c75a73decb/1556564892816-7PIBFHDPYNI1M8LQ9XEK/ke17ZwdGBToddI8pDm48kGfiFqkITS6axXxhYYUCnlRZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxQ1ibo-zdhORxWnJtmNCajDe36aQmu-4Z4SFOss0oowgxUaachD66r8Ra2gwuBSqM/unnamed-1.png"
+            }
+        ]
+    },
+    vcs: {
+        title: "VC\'s",
+        resources: [
+            {
+                name: "Brown Angel Group",
+                url: "https://www.brownangelgroup.org/",
+                image: "https://media-exp1.licdn.com/dms/image/C4E0BAQHJEUNTUK_nEg/company-logo_200_200/0?e=2159024400&v=beta&t=T4iU8f57bj78JsrJc7CBT_KtXJVAbuacHgESnmaIRQE"
+            },
+            {
+                name: "Waterman Ventures",
+                url: "https://www.avgfunds.com/waterman-ventures/",
+                image: "https://res-2.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/bfwrt3aongizbab9xckw"
+            },
+            {
+                name: "Unshackled Ventures",
+                url: "https://www.unshackledvc.com/",
+                image: "https://res-5.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco/akh4zfkbhqbj44wre4sd"
+            }
+        ]
+    },
+    largeGrants: {
+        title: "Large Grants",
+        resources: [
+            {
+                name: "Thiel Fellowship",
+                url: "https://thielfellowship.org/",
+                image: "https://pbs.twimg.com/media/DCdkWp8UwAAUfsw.jpg"
+            },
+            {
+                name: "1517 Fund",
+                url: "https://www.1517fund.com/",
+                image: "https://miro.medium.com/fit/c/160/160/1*DbuUr7RFgks-foWKhwSguw.png"
+            },
+            {
+                name: "Brown Venture Prize",
+                url: "https://entrepreneurship.brown.edu/the-brown-venture-prize/",
+                image: "https://img.evbuc.com/https%3A%2F%2Fcdn.evbuc.com%2Fimages%2F54510189%2F190855080674%2F1%2Foriginal.20190102-180018?h=200&w=450&auto=compress&rect=0%2C13%2C2196%2C1098&s=6694daca2b2bbc9fbe4d0d5b9ebf39be"
+            }
+        ]
+    },
+    smallGrants: {
+        title: "Small Grants",
+        resources: [
+            {
+                name: "Explore Grant",
+                url: "https://ufunds.brown.edu/apply/browse",
+                image: "https://www.brown.edu/research/misc/favicon.ico"  //shield
+            },
+            {
+                name: "Expand Grants",
+                url: "https://ufunds.brown.edu/apply/browse", //Seems to be the same link as before
+                image: "https://www.brown.edu/research/misc/favicon.ico" //shield
+            },
+            {
+                name: "Maker Grant",
+                url: "http://www.browndesignworkshop.org/grants/",
+                image: "https://images.squarespace-cdn.com/content/56d3555ab6aa606b54a4e049/1570057892768-R2B3XU9TLIUFXXYN81H3/3-22_Full_Blue_Logo_Transparent.png?format=1500w&content-type=image%2Fpng"
+            }
+        ]
     }
     // ...
 }
