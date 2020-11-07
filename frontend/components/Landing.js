01c0a3da157d43a767e5e7a2830902ec464fd703
nathanluu <nate1299@me.com>
2020-11-07 09:03:27 -0800

    Minor style changes

--- frontend/components/Landing.js
+++ frontend/components/Landing.js
@@ -11,8 +11,8 @@ export default function () {
             <p className="text-xl text-gray-500">A guide to help Brown and RISD students navigate the entrepreneurial resources on College Hill.</p>
             <div className="mt-4">
                 <Link href="/survey">
-                    <a className="bg-red-600 block md:inline-block rounded-lg py-3 font-display transition-all duration-200 font-medium hover:bg-red-400 focus:bg-red-400 px-12 mt-8 text-white">
-                        Let's get started &rarr;
+                    <a className="bg-red-600 block md:inline-block font-medium text-lg rounded-lg py-3 font-display transition-all duration-200 font-medium hover:bg-red-400 focus:bg-red-400 px-12 mt-8 text-white">
+                        Let's get started
                     </a>
                 </Link>
             </div>
