dcfc9640f361dc8760c62d0f75cdcefd8a788242
nathanluu <nate1299@me.com>
2020-11-06 19:22:08 -0800

    logo update

--- frontend/components/Navbar.js
+++ frontend/components/Navbar.js
@@ -4,13 +4,11 @@ export default function Navbar({isLanding}) {
     const linkStyling = `text-gray-500 hover:text-red-600 focus:text-red-500 ${!isLanding && "hidden sm:inline"}`
 
     return <div className="absolute top-0 right-0 p-4 w-full flex justify-between items-center font-display">
-        {isLanding ? <a href="#">Brown EP</a> :
-            <Link href="/">
-                <a className="font-medium text-gray-800 text-xl">
-                    Venture<span className="font-sans text-gray-400 font-light">@</span>Brown
-                </a>
-            </Link>}
-
+        <Link href="/">
+            <a className="font-medium text-gray-800 text-xl">
+                Venture<span className="font-sans text-gray-400 font-light">@</span>Brown
+            </a>
+        </Link>
         <div className="space-x-4">
             <Link href="/about">
                 <a className={linkStyling}>About</a>
