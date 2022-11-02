35441d2c31d5e019c4fc8ea6d10afe2ad791cabb
Alexis Fry <afry477@gmail.com>
2022-11-01 20:41:22 -0400

    admin login frontend

--- frontend/pages/adminLogin.js
+++ frontend/pages/adminLogin.js
@@ -17,16 +17,23 @@ class Login extends React.Component{
     }
     handleSubmit = (e) =>{
         e.preventDefault()
+        alert("logged in!!!!!!!!!!!!!")
     }
     render(){
         return(
             <div>
-                <img src="/bears/smiling_red_bear.svg" className="rounded-lg mb-6 h-64 mx-auto"/>
-                <form onSubmit = {this.handleSubmit} >
-                    <input type="email" name="email" placeholder="Email" required/>
-                    <input type="password" name="password" placeholder="Password" required/>
-                    <button onSubmit = {this.handleSubmit}>Log In</button>
-                </form>
+                <Navbar isLanding/>
+
+                <img src="/bears/smiling_red_bear.svg" className="rounded-lg mb-6 h-64 mx-auto" style={{top: '50px', position: 'relative'}}/>
+
+                <div style={{top: '50px', position: 'relative'}}>
+                    <div className="text-center text-3xl sm:text-5xl font-bold text-gray-900"> Admin Login </div>
+                    <form onSubmit = {this.handleSubmit} className="font-sans text-gray-400 font-light login-box" >
+                        <input className="login-input" type="email" name="email" placeholder="Email" required/>
+                        <input className="login-input" type="password" name="password" placeholder="Password" required/>
+                        <button className="bg-red-600 block md:inline-block font-medium text-lg rounded-lg py-3 font-display transition-all duration-200 font-medium hover:bg-red-400 focus:bg-red-400 px-12 mt-8 text-white" onSubmit = {this.handleSubmit}>Log In</button>
+                    </form>
+                </div>
             </div>
         )
     }
